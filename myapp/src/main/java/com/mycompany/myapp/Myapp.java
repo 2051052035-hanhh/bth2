4cf1b13dbed60a2fa4e195e522e952e85bb697ea
Mai Lien My Hanh <2051052035hanh@ou.edu.vn>
2023-02-16 17:08:12 +0700

    cap nhat chuc nang form

--- myapp/src/main/java/com/mycompany/myapp/Myapp.java
+++ myapp/src/main/java/com/mycompany/myapp/Myapp.java
@@ -12,6 +12,8 @@ package com.mycompany.myapp;
 public class Myapp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch ();
+        int a =10;
+        if (a>5) a+=1;
     }
 }
